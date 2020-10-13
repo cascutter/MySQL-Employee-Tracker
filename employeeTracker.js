eeeec7c81def666d1f2198afc10f6718fd952f1b
cascutter <cascutter13@gmail.com>
2020-10-13 09:18:09 -0500

    Added first set of choices and switch statement for the user

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 const config = require("config.json")("./config.json");
 const inquirer = require("inquirer");
-const consoleTable = require("console.table");
+const conTable = require("console.table");
 
 const con = mysql.createConnection(config);
 
@@ -9,3 +9,73 @@ con.connect(function(err) {
     if(err) throw(err);
     console.log("Connected!");
 });
+
+function askUser() {
+    inquirer.prompt(
+        {
+            type: "list",
+            name: "askUser",
+            message: "What would you like to do?",
+            choices: [
+                "View all employees",
+                "View all roles",
+                "View all departments",
+                "Add employee",
+                "Add role",
+                "Add department",
+                "Update employee role",
+                "Remove employee"
+            ]
+        }).then(function(answer) {
+            console.log(answer);
+            switch (answer.name) {
+                case "View all employees":
+                    viewAll();
+                    break;
+
+                case "View all roles":
+                    viewRoles();
+                    break;
+
+                case "View all departments":
+                    viewDepartments();
+                    break;
+
+                case "Add employee":
+                    addEmployee();
+                    break;
+                
+                case "Add role":
+                    addRole();
+                    break;
+
+                case "Add department":
+                    addDepartment();
+                    break;
+
+                case "Update employee role":
+                    updateRole();
+                    break;
+
+                case "Remove employee":
+                    removeEmployee();
+                    break;
+            }
+        })
+} askUser();
+
+// function viewAll()
+
+// function viewRoles()
+
+// function viewDepartments()
+
+// function addEmployee()
+
+// function addRole()
+
+// function addDepartment()
+
+// function updateRole()
+
+// function removeEmployee()
